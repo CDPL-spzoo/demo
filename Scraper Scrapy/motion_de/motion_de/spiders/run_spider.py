3ea9f27ee58b26d2bb4721babd8542c70836f0b6
aaprotasov <89977669+aaprotasov@users.noreply.github.com>
2023-09-18 21:44:40 +0300

    Delete Scraper motoin folder

--- Scraper Scrapy/motion_de/motion_de/spiders/run_spider.py
+++
@@ -1,9 +0,0 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from motoin_spider import MarketSpider
-
-
-if __name__ == '__main__':
-    process = CrawlerProcess(get_project_settings())
-    process.crawl(MarketSpider)
-    process.start()
